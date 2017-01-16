6a288e84df4107cef1ccb25c6ad386945e313dc1
grillazz <the@grillazz.com>
2017-01-16 17:42:41 +0100

    simple unit tests

--- crazywheels/tests/test_views.py
+++ crazywheels/tests/test_views.py
@@ -1,6 +1,8 @@
 from rest_framework.reverse import reverse
 from rest_framework.test import APITestCase
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED
+
+from factory import build as factory_build
 
 from .factories import MessageFactory
 from crazywheels.serializers import MessageSerializer
@@ -9,6 +11,15 @@ import pytest
 
 
 class MessageApiTests(APITestCase):
+
+    def setUp(self):
+        self.api_data = factory_build(dict, FACTORY_CLASS=MessageFactory)
+
+    @pytest.mark.django_db
+    def test_message_create(self):
+        api_url = reverse('message-list')
+        api_response = self.client.post(api_url, data=self.api_data)
+        self.assertEqual(api_response.status_code, HTTP_201_CREATED)
 
     @pytest.mark.django_db
     def test_message_list(self):

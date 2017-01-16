9d47f13925cd8763f75ed660f0401de212fbffb4
grillazz <the@grillazz.com>
2017-01-16 10:40:54 +0100

    message admin added

--- crazywheels/admin.py
+++ crazywheels/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import MessageModel
+
+
 # Register your models here.
+class MessageAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(MessageModel, MessageAdmin)

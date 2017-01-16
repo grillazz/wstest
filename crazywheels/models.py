3c250b0787bbf24b462ae45a0e2e644aada19968
grillazz <the@grillazz.com>
2017-01-16 10:32:10 +0100

    message model added

--- crazywheels/models.py
+++ crazywheels/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class MessageModel(models.Model):
+    title = models.CharField(max_length=255)
+    message = models.TextField()
+    image = models.ImageField()

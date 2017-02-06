436ac5d97ee61f8835afa8bdcefaba1217c88b70
grillazz <the@grillazz.com>
2017-02-06 11:31:23 +0100

    update docker setup for travis CI

--- crazywheels/migrations/0001_initial.py
+++ crazywheels/migrations/0001_initial.py
@@ -14,7 +14,7 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
-            name='MessageModel_QAZ',
+            name='MessageModel',
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=255)),

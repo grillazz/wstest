7ff639040b3db755811c6230a00af9e5e2903755
grillazz <the@grillazz.com>
2017-02-21 10:13:21 +0100

    JWT auth tests

--- crazywheels/tests/test_views.py
+++ crazywheels/tests/test_views.py
@@ -1,6 +1,8 @@
 from rest_framework.reverse import reverse
-from rest_framework.test import APITestCase
+from rest_framework.test import APITestCase, APIClient
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED
+
+from rest_framework_jwt import utils
 
 from .factories import MessageFactory, MessageModel, UserFactory
 
@@ -9,6 +11,7 @@ from ..serializers import MessageSerializer
 
 class MessageApiTests(APITestCase):
     def setUp(self):
+        self.csrf_client = APIClient(enforce_csrf_checks=False)
         self.user = UserFactory()
         self.message = MessageFactory.build()
         self.message_data = MessageSerializer(self.message).data
@@ -19,34 +22,68 @@ class MessageApiTests(APITestCase):
             'username': self.user.username,
             'password': UserFactory.DEFAULT_PASSWORD
         }
+        self.token = self.csrf_client.post(self.jwt_url, self.user_data, format='json')
+        self.token = self.token.data['token']
 
-    def obtain_token(self):
-        #self.client.login(username=self.user.username, password=UserFactory.DEFAULT_PASSWORD)
-        response = self.client.post(self.jwt_url, self.user_data, format='json')
-        print(response.data['token'])
 
+    def test_123(self):
+        response = self.csrf_client.post(self.jwt_url, self.user_data, format='json')
+        decoded_payload = utils.jwt_decode_handler(response.data['token'])
+        self.assertEqual(response.status_code, HTTP_200_OK)
+        self.assertEqual(decoded_payload['username'], self.user.username)
+        self.csrf_client.credentials(HTTP_AUTHORIZATION='JWT ' + response.data['token'])
+        print("Authorization: JWT " + response.data['token'])
+        api_response = self.csrf_client.get(self.message_url)
+        self.assertEqual(api_response.status_code, HTTP_200_OK)
+        pass
+
+
+    #     def test_jwt_login_custom_response_json(self):
+    #         """
+    #         Ensure JWT login view using JSON POST works.
+    #         """
+    #         client = APIClient(enforce_csrf_checks=True)
+    #
+    #         response = client.post('/auth-token/', self.data, format='json')
+    #
+    #         decoded_payload = utils.jwt_decode_handler(response.data['token'])
+    #
+    #         self.assertEqual(response.status_code, status.HTTP_200_OK)
+    #         self.assertEqual(decoded_payload['username'], self.username)
+    #
+    # self.assertEqual(response.data['user'], self.username)
 
     # def create_message(self):
-    #     return self.client.post(self.api_url, self.api_data, format='json')
-
-    def test_message_create(self):
-        api_response = self.obtain_token()
-        # self.assertEqual(api_response.status_code, HTTP_201_CREATED)
-        # self.assertEqual(MessageModel.objects.count(), 1)
-        # self.assertEqual(MessageModel.objects.get().title, 'New Message for JJ')
+    #     response = self.client.post(self.jwt_url, self.user_data, format='json')
+    #     print(response.data['token'])
+    #     self.client.credentials(HTTP_AUTHORIZATION='Authorization: JWT ' + response.data['token'])
+    #     return self.client.post(self.message_url, self.message_data, format='json')
+    #
+    # def test_message_create(self):
+    #     api_response = self.create_message()
+    #     self.assertEqual(api_response.status_code, HTTP_201_CREATED)
+    #     self.assertEqual(MessageModel.objects.count(), 1)
+    #     self.assertEqual(MessageModel.objects.get().title, 'New Message for JJ')
 
     # def test_message_list(self):
-    #     self.create_message()
-    #     api_response = self.client.get(self.api_url)
+    #      = self.client.post(self.jwt_url, self.user_data, format='json')
+    #     header = {'HTTP_AUTHORIZATION': 'Authorization: JWT {}'.format(response.data['token'])}
+    #     api_response = self.client.get(self.message_url, {}, **header)
     #     self.assertEqual(api_response.status_code, HTTP_200_OK)
 
+    # def test_authorization(self):
+    #     header = {'HTTP_AUTHORIZATION': 'Token {}'.format(self.token)}
+    #     response = self.client.get(reverse('bookmark-list'), {}, **header)
+    #     self.assertEqual(response.status_code, 200, "REST token-auth failed")
+
+    #
     # def test_message_get(self):
     #     self.create_message()
     #     api_response = self.client.get('/api/v1/message/1/')
     #     self.assertEqual(api_response.status_code, HTTP_200_OK)
     #     self.assertEqual(api_response.data,
     #                      {'image': None, 'message': 'How are you Dude?', 'title': 'New Message for JJ'})
-
+    #
     # def test_message_detail(self):
     #     self.create_message()
     #     created_message = MessageModel.objects.get()
@@ -54,5 +91,5 @@ class MessageApiTests(APITestCase):
     #         reverse('message-detail', kwargs={'pk': created_message.pk})
     #     )
     #     self.assertEqual(api_response.status_code, HTTP_200_OK)
-    #     self.assertEqual(api_response.data['title'], self.api_data['title'])
-    #     self.assertEqual(api_response.data['message'], self.api_data['message'])
+    #     self.assertEqual(api_response.data['title'], self.message_data['title'])
+    #     self.assertEqual(api_response.data['message'], self.message_data['message'])
--- crazywheels/views.py
+++ crazywheels/views.py
@@ -5,10 +5,16 @@ from rest_framework.viewsets import ModelViewSet
 from .models import MessageModel
 from .serializers import MessageSerializer
 
+from rest_framework.permissions import IsAuthenticated
+
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+
 
 # REsT API endpoint views
 class MessageViewSet(ModelViewSet):
     """ViewSet for the Message class"""
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (JSONWebTokenAuthentication,)
 
     queryset = MessageModel.objects.all()
     serializer_class = MessageSerializer
